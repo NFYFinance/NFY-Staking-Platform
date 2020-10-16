d9a26f3ee7c2179dfe8ecd46d903e524e4fb1754
c-note <58487146+dev-Cnote@users.noreply.github.com>
2020-10-16 22:56:53 +0100

    Update demoToken.sol

--- contracts/demoToken.sol
+++ contracts/demoToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Uniswapcap is ERC20  {  
+contract Demo is ERC20  {  
 
     constructor() public ERC20("DEMO", "DMT") {
             _mint(_msgSender(), 100000 * (10 ** uint256(decimals())));
